f979442f3f5b3ebe6c0b2666d66292d0237b0572
mio-556 <svancara.michall@gmail.com>
2025-03-17 19:04:44 +0100

    CORS policy init resolution

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ import express from "express";
 import runsRoute from "./routes/api/runs.js";
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle preflight OPTIONS requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 // Defining the routes
 app.get("/", (request, response) => {
   response.send("<h3>Express.js server.</h3>");
